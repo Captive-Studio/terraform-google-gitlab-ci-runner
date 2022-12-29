0a459544b28ee34de5e7b88fc5d9b9843f4bc380
Julien Polo <julien.polo@gmail.com>
2022-12-29 14:15:44 +0100

    ✨ Add service_account_agent, service_account_runner output

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
+output "service_account_agent" {
+  value       = google_service_account.agent
+  description = "Agent Service account"
+}
+
+output "service_account_runner" {
+  value       = service_account_runner.runner
+  description = "Runner Service account"
+}
 
